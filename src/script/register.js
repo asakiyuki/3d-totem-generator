caefc412db4956ddd784ad6c9ce1880fe007df70
AsakiZuki <vantrong2007vn@gmail.com>
2023-08-28 00:48:34 +0700

    idk

--- src/script/register.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator && !navigator.isStandalone) {
-    document.getElementById('installButton').style.display = 'block';
-    document.getElementById('installButton').addEventListener('click', () => {
-        navigator.serviceWorker.register('./sw.js');
-    })
-}
