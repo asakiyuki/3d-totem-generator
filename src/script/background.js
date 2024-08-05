6b09dbd02fbed2621c0dee36beabc685fbc37edb
Asaki Yuki <108646953+asakiyuki@users.noreply.github.com>
2024-08-06 01:28:59 +0700

    Update background.js

--- src/script/background.js
+++ src/script/background.js
@@ -1,7 +1,7 @@
 {
     updateBackground = () => {
         const { clientWidth: x, clientHeight: y } = document.body, stl = document.body.style;
-        stl.background = `linear-gradient(to bottom, #0003, #000B), url('./src/texture/${(x < y) ? 'ver' : 'hoz'}-background.png')`;
+        stl.background = `linear-gradient(to bottom, #0003, #000B), url('${(x < y) ? 'https://asakiyuki.vercel.app/textures/3d-totem/firefly_mo.jpg' : 'https://asakiyuki.vercel.app/textures/3d-totem/firefly_pc.jpeg'}')`;
         stl.backgroundPosition = 'top';
         stl.backgroundSize = 'cover';
         stl.backgroundAttachment = 'fixed';
